d5f4fbcab2f9793d19fcd1b41d2a897c09ce442b
Damien Huzard <48721374+dhuzard@users.noreply.github.com>
2026-09-10 17:33:25 +0200

    Add playground petition configuration

---
+++ public/aires-jeux-montpellier/config.js
@@ -0,0 +1,8 @@
+export const SITE_CONFIG = {
+  supabaseUrl: "YOUR_SUPABASE_URL",
+  supabaseAnonKey: "YOUR_SUPABASE_ANON_KEY",
+  turnstileSiteKey: "YOUR_TURNSTILE_SITE_KEY",
+  ownerName: "Damien Huzard",
+  ownerEmail: "damien.huzard@gmail.com",
+  showPublicSupporters: true
+};
